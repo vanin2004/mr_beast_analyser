000344d20eac3e1d735f055a3e393a2476c69aed
vanin2004 <160011677+vanin2004@users.noreply.github.com>
2026-03-27 21:32:43 +0300

    Initial commit

---
+++ t_normalize.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from normalize import normalize_column
+
+
+def sample_df():
+    return pd.DataFrame({
+        'timestamp': [0, 3, 5, 9],
+        'views': [10, None, 30, 0],
+        'likes': [1, 2, None, 4]
+    })
+
+
+def run_examples():
+    df = sample_df()
+    print('Input:')
+    print(df)
+    res = normalize_column(df, x='timestamp', y=['views', 'likes'], x0=0, step=1, xmax=10)
+    print('\nResult:')
+    print(res)
+
+
+if __name__ == '__main__':
+    run_examples()
+
